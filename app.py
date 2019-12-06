35478e316589f42c716bdb74f26d5ab4b9c51df4
gautambitious <mvgt.gautam@gmail.com>
2019-12-06 10:57:04 +0530

    Application Complete

--- app.py
+++ app.py
@@ -17,13 +17,51 @@ class Todo(db.Model):
         return '<Task %r>' % self.id
 
 
+# noinspection PyBroadException
 @app.route("/", methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
-        pass
+        task_content = request.form['content']
+        new_task = Todo(content=task_content)
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect("/")
+        except:
+            return "There was an issue adding your task"
     else:
-        task = Todo.query.order_by(Todo.date_created).all()
-        return render_template("index.html", task=task)
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template("index.html", tasks=tasks)
+
+
+@app.route("/delete/<int:id>")
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect("/")
+
+    except:
+        return "There was an error deleting the Task"
+
+
+@app.route("/update/<int:id>", methods=['GET', 'POST'])
+def update(id):
+    task = Todo.query.get_or_404(id)
+
+    if request.method == "POST":
+        task.content = request.form['content']
+
+        try:
+            db.session.commit()
+            return redirect("/")
+        except:
+            return "Could not update Task"
+    else:
+        return render_template("update.html", task=task)
 
 
 if __name__ == "__main__":
